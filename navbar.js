eecd550e5a8d9e7d4984d49500b4c1730e91b46b
chanrey korn <kornchanrey513@gmail.com>
2023-07-24 21:59:28 +0700

    chanrey-korn

---
+++ navbar.js
@@ -0,0 +1,15 @@
+let facebook = document.getElementById("fb");
+let youtbube = document.getElementById("yb");
+let gmail = document.getElementById("ep");
+
+facebook.addEventListener('click', function(){
+    window.location = 'https://web.facebook.com/profile.php?id=100070704646897';
+});
+
+youtbube.addEventListener('click', function(){
+    window.location = 'https://web.facebook.com/profile.php?id=100070704646897';
+});
+
+gmail.addEventListener('click', function(){
+    window.location = 'https://web.facebook.com/profile.php?id=100070704646897';
+});
